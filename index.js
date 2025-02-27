d7c9ca62eba08a407c3fb54c0f9b20fbda479218
unknown <Everett.Horton95@yahoo.com>
2025-02-26 23:16:24 -0500

    updated freelancers

--- index.js
+++ index.js
@@ -13,20 +13,45 @@ let freelancers = [
 
 // calculate the average starting price
 function calculateAveragePrice(freelancers) {
-    const total = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 2);
-    return total / freelancers.length;
+    const total = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 0);
+    return (total / freelancers.length).toFixed(2);
 }
+const container = document.getElementById("freelancer-container");
+const averaragePriceDisplay =document.getElementById("average-price")
 
 function renderFreeLancers(){
-    const container = document.getbyElementById("freelancer-container");
+    
     container.innerHTML = ""; // clear previous freelancers
 }
 
 freelancers.forEach(freelancers => {
     const freelancerDiv = document.createElement("div");
-    freelancerDiv.innerHTML =
+    freelancerDiv.innerHTML = `
+    <p><strong> ${freelancers.name}<strong></p>
+    <p> ${freelancers.occupation} </p>
+    <p>Starting Price: $${freelancers.price}</p>
+     `
 
   container.appendChild(freelancerDiv);
+});
+
+    // Update the average price display
+     averaragePriceDisplay.textContent = calculateAveragePrice();
+
+function addRandomFreelancers() {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    const randomOccupation = occupations[Math.floor(Math.random() * occupations.length)];
+    const randomPrice = prices [Math.floor(Math.randdom() * prices.length)];
+
+    freelancers.push({name: randomName, occupation: randdomOccupation, price: randomPrice});
+    renderFreeLancers();
 }
 
-)
+// Initail render
+renderFreeLancers();
+
+// Add a new freelancer every 5 seconds
+setInterval(addRandomFreelancers, 5000);
+
+console.log(addRandomFreelancers)
+
